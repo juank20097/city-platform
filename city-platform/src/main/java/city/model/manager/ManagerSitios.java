3c0af3dce4c711258a348f67c736da89e7a49e70
juank20097 <juank20097@gmail.com>
2016-05-18 15:06:44 -0500

    -generacion de metodos, vistas y controler

--- city-platform/src/main/java/city/model/manager/ManagerSitios.java
+++ city-platform/src/main/java/city/model/manager/ManagerSitios.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import city.model.dao.entidades.GenArea;
 import city.model.dao.entidades.GenCatalogoItem;
 import city.model.dao.entidades.GenSectore;
 
@@ -60,14 +61,14 @@ public class ManagerSitios {
 	 * 
 	 * @return
 	 */
-	public Integer cargarId(){
-		Integer id=0;
+	public Integer sectorId() {
+		Integer id = 0;
 		try {
 			List<GenSectore> ls = this.findAllsectores();
-			if (ls==null || ls.size()==0){
-				id=1;
-			}else{
-				id=ls.size()+1;
+			if (ls == null || ls.size() == 0) {
+				id = 1;
+			} else {
+				id = ls.size() + 1;
 			}
 			return id;
 		} catch (Exception e) {
@@ -76,7 +77,7 @@ public class ManagerSitios {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Metodo para ingresar un Atributo a la base de datos
 	 * 
@@ -89,7 +90,7 @@ public class ManagerSitios {
 			String descripcion) throws Exception {
 		try {
 			GenSectore sector = new GenSectore();
-			sector.setSecId(this.cargarId());
+			sector.setSecId(this.sectorId());
 			sector.setSecNombre(nombre);
 			sector.setSecDireccionReferencial(direccion);
 			sector.setSecDescripcion(descripcion);
@@ -113,7 +114,7 @@ public class ManagerSitios {
 	 * @throws Exception
 	 */
 	public void editarSector(Integer id, String nombre, String direccion,
-			String descripcion,String estado) throws Exception {
+			String descripcion, String estado) throws Exception {
 		try {
 			GenSectore sector = this.SectoresByID(id);
 			sector.setSecNombre(nombre);
@@ -129,8 +130,131 @@ public class ManagerSitios {
 		}
 	}// Cierre del metodo
 	
+	/**
+	 * Metodo para listar
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<GenSectore> AllSectoresActivos() {
+		List<GenSectore> l = mngDao.findWhere(GenSectore.class,
+				"o.secEstado='A'", null);
+			return l;
+	}// Cierre del metodo
+
+	// //////////////////////////////////////////////////////////(AREAS)/////////////////////////////////////////////////////////////////////
+	/**
+	 * Creación de metodos para el manejo de la tabla AREAS
+	 * 
+	 */
+
+	/**
+	 * Metodo para listar todas los datos existentes
+	 * 
+	 * @return La lista de todas los datos encontradas
+	 */
+	@SuppressWarnings("unchecked")
+	public List<GenArea> findAllAreas() throws Exception {
+		return mngDao.findAll(GenArea.class);
+	}// Cierre del metodo
+
+	/**
+	 * Metodo para obtener el Atributo mediante un ID
+	 * 
+	 * @param dni
+	 * @return Objeto
+	 * @throws Exception
+	 */
+	public GenArea AreaByID(Integer dni) throws Exception {
+		return (GenArea) mngDao.findById(GenArea.class, dni);
+	}// Cierre del metodo
+
+	/**
+	 * Metodo para generar el id
+	 * 
+	 * @return
+	 */
+	public Integer areaId() {
+		Integer id = 0;
+		try {
+			List<GenArea> l = this.findAllAreas();
+			if (l == null || l.size() == 0) {
+				id = 1;
+			} else {
+				id = l.size() + 1;
+			}
+			return id;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Metodo para ingresar un Atributo a la base de datos
+	 * 
+	 * @param sector
+	 * @param nombre
+	 * @param descripcion
+	 * @param latitud
+	 * @param longitud
+	 * @param padre
+	 * @throws Exception
+	 */
+	public void insertarArea(Integer sector, String nombre, String descripcion,
+			String latitud, String longitud, String padre) throws Exception {
+		try {
+			GenArea area = new GenArea();
+			area.setAreId(this.areaId());
+			area.setGenSectore(this.SectoresByID(sector));
+			area.setAreNombre(nombre);
+			area.setAreDescripcion(descripcion);
+			area.setAreLatitud(latitud);
+			area.setAreLongitud(longitud);
+			area.setArePadre(padre);
+			area.setAreEstado("A");
+			mngDao.insertar(area);
+			System.out.println("Bien_insertar_area");
+		} catch (Exception e) {
+			System.out.println("Error_insertar_area");
+			e.printStackTrace();
+		}
+	}// Cierre del metodo
+
+	/**
+	 * Metodo para editar un Atributo en la base de datos
+	 * 
+	 * @param area
+	 * @param sector
+	 * @param nombre
+	 * @param descripcion
+	 * @param latitud
+	 * @param longitud
+	 * @param padre
+	 * @throws Exception
+	 */
+	public void editarArea(Integer area,Integer sector, String nombre, String descripcion,
+			String latitud, String longitud, String padre, String estado) throws Exception {
+		try {
+			GenArea are = this.AreaByID(area);
+			are.setGenSectore(this.SectoresByID(sector));
+			are.setAreNombre(nombre);
+			are.setAreDescripcion(descripcion);
+			are.setAreLatitud(latitud);
+			are.setAreLongitud(longitud);
+			are.setArePadre(padre);
+			are.setAreEstado(estado);
+			mngDao.actualizar(are);
+			System.out.println("Bien_mod_area");
+		} catch (Exception e) {
+			System.out.println("Error_mod_area");
+			e.printStackTrace();
+		}
+	}// Cierre del metodo
+
 	// //////////////////////////////////////////////////////////(Items)/////////////////////////////////////////////////////////////////////
-	
+
 	/**
 	 * Metodo para listar
 	 * 
