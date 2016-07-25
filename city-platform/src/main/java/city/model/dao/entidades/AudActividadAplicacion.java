872350172be8262de56ca8d5a60c8c8d91f9b01f
ricardo <ricardo@desarrollo>
2016-07-25 17:01:44 -0500

    Dependencias para interceptor de hibernate Agregar entidad e interceptor Archivo de configuración para la base auditoria

---
+++ city-platform/src/main/java/city/model/dao/entidades/AudActividadAplicacion.java
@@ -0,0 +1,133 @@
+package city.model.dao.entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the aud_actividad_aplicacion database table.
+ * 
+ */
+@Entity
+@Table(name="aud_actividad_aplicacion")
+@NamedQuery(name="AudActividadAplicacion.findAll", query="SELECT a FROM AudActividadAplicacion a")
+public class AudActividadAplicacion implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="apl_id")
+	@GeneratedValue
+	private Integer aplId;
+
+	@Column(name="apl_aplicacion")
+	private String aplAplicacion;
+
+	@Column(name="apl_aplicacion_cliente")
+	private String aplAplicacionCliente;
+
+	@Column(name="apl_fecha")
+	private Timestamp aplFecha;
+
+	@Column(name="apl_ip")
+	private String aplIp;
+
+	@Column(name="apl_origen_archivo")
+	private String aplOrigenArchivo;
+
+	@Column(name="apl_sentencia")
+	private String aplSentencia;
+
+	@Column(name="apl_tabla")
+	private String aplTabla;
+
+	@Column(name="apl_tipo_operacion")
+	private String aplTipoOperacion;
+
+	@Column(name="apl_usuario_aplicacion")
+	private String aplUsuarioAplicacion;
+
+	public AudActividadAplicacion() {
+	}
+
+	public Integer getAplId() {
+		return this.aplId;
+	}
+
+	public void setAplId(Integer aplId) {
+		this.aplId = aplId;
+	}
+
+	public String getAplAplicacion() {
+		return this.aplAplicacion;
+	}
+
+	public void setAplAplicacion(String aplAplicacion) {
+		this.aplAplicacion = aplAplicacion;
+	}
+
+	public String getAplAplicacionCliente() {
+		return this.aplAplicacionCliente;
+	}
+
+	public void setAplAplicacionCliente(String aplAplicacionCliente) {
+		this.aplAplicacionCliente = aplAplicacionCliente;
+	}
+
+	public Timestamp getAplFecha() {
+		return this.aplFecha;
+	}
+
+	public void setAplFecha(Timestamp aplFecha) {
+		this.aplFecha = aplFecha;
+	}
+
+	public String getAplIp() {
+		return this.aplIp;
+	}
+
+	public void setAplIp(String aplIp) {
+		this.aplIp = aplIp;
+	}
+
+	public String getAplOrigenArchivo() {
+		return this.aplOrigenArchivo;
+	}
+
+	public void setAplOrigenArchivo(String aplOrigenArchivo) {
+		this.aplOrigenArchivo = aplOrigenArchivo;
+	}
+
+	public String getAplSentencia() {
+		return this.aplSentencia;
+	}
+
+	public void setAplSentencia(String aplSentencia) {
+		this.aplSentencia = aplSentencia;
+	}
+
+	public String getAplTabla() {
+		return this.aplTabla;
+	}
+
+	public void setAplTabla(String aplTabla) {
+		this.aplTabla = aplTabla;
+	}
+
+	public String getAplTipoOperacion() {
+		return this.aplTipoOperacion;
+	}
+
+	public void setAplTipoOperacion(String aplTipoOperacion) {
+		this.aplTipoOperacion = aplTipoOperacion;
+	}
+
+	public String getAplUsuarioAplicacion() {
+		return this.aplUsuarioAplicacion;
+	}
+
+	public void setAplUsuarioAplicacion(String aplUsuarioAplicacion) {
+		this.aplUsuarioAplicacion = aplUsuarioAplicacion;
+	}
+
+}
---
+++ city-platform/src/main/java/city/model/manager/ActivityTrackerInterceptor.java
@@ -0,0 +1,178 @@
+package city.model.manager;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.sql.Timestamp;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.Table;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.event.spi.PostUpdateEvent;
+import org.hibernate.type.Type;
+import org.json.simple.JSONObject;
+
+import city.controller.access.SesionBean;
+import city.model.dao.entidades.AudActividadAplicacion;
+
+@SessionScoped
+@ManagedBean
+public class ActivityTrackerInterceptor extends EmptyInterceptor {
+
+	private static final long serialVersionUID = 1L;
+	private Set inserts = new HashSet();
+	private Set updates = new HashSet();
+	private Set deletes = new HashSet();
+	boolean isMainEntity = false;
+
+	// called when record deleted.
+	public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+		deletes.add(entity);
+		isMainEntity = true;
+		
+	}
+
+	// called when record updated
+	public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState,
+			String[] propertyNames, Type[] types) {
+
+		if (!(entity instanceof AudActividadAplicacion)) {
+			updates.add(entity);
+			isMainEntity = true;
+			return true;
+		}
+		isMainEntity = false;
+		return false;
+	}
+
+	// called on load events
+	public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+
+		// log loading events
+
+		return true;
+	}
+
+	public void onPostUpdate(PostUpdateEvent postUpdateEvent) {
+
+		if (!(postUpdateEvent.getEntity() instanceof AudActividadAplicacion)) {
+			
+			isMainEntity = true;
+
+		}
+		isMainEntity = false;
+
+	}
+
+	public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+
+		if (!(entity instanceof AudActividadAplicacion)) {
+
+			inserts.add(entity);
+			isMainEntity = true;
+			
+			return true;
+		}
+
+		isMainEntity = false;
+		return false;
+	}
+
+	// called before commit into database
+	public void preFlush(Iterator iterator) {
+		
+	}
+
+	// called after committed into database
+	public void postFlush(Iterator iterator) {
+
+		try {
+			HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext()
+					.getSession(false);
+			SesionBean user = (SesionBean) session.getAttribute("sesionBean");
+
+			String userName = user.getUsuario();
+
+			HttpServletRequest httpServletRequest = (HttpServletRequest) FacesContext.getCurrentInstance()
+					.getExternalContext().getRequest();
+			Configuration config = new Configuration();
+			config.configure();
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties());
+			SessionFactory sessionFactory = configuration.buildSessionFactory(ssrb.build());
+			Session sessionn = sessionFactory.openSession();
+
+			String IP = httpServletRequest.getRemoteAddr();
+			String appName = "YCP";
+			String client = httpServletRequest.getHeader("User-Agent");
+			Timestamp fecha = new Timestamp(new java.util.Date().getTime());
+			generateAudit(inserts, userName, client, IP, appName, "INSERT", fecha, sessionn);
+			generateAudit(updates, userName, client, IP, appName, "UPDATE", fecha, sessionn);
+			generateAudit(deletes, userName, client, IP, appName, "DELETE", fecha, sessionn);
+
+			sessionFactory.close();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+			inserts.clear();
+			updates.clear();
+			deletes.clear();
+
+		}
+
+	}
+
+	private void generateAudit(Set iterator, String userName, String client, String IP, String appName, String action,
+			Timestamp fecha, Session session) {
+		for (Iterator it = iterator.iterator(); it.hasNext();) {
+			Object entity = it.next();
+			Class<? extends Object> c = entity.getClass();
+			String tableName = entity.getClass().getAnnotation(Table.class).name();
+			JSONObject jsonObject = new JSONObject();
+
+			for (Field field : c.getDeclaredFields()) {
+				field.setAccessible(true);
+				String name = field.getName();
+				String value = "";
+				try {
+					value = String.valueOf(field.get(entity));
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+				jsonObject.put(name, value);
+			}
+
+			AudActividadAplicacion audit = new AudActividadAplicacion();
+			audit.setAplUsuarioAplicacion(userName);
+			audit.setAplAplicacion(appName);
+			audit.setAplAplicacionCliente(client);
+			audit.setAplFecha(fecha);
+			audit.setAplIp(IP);
+			audit.setAplOrigenArchivo(c.getName());
+			audit.setAplSentencia(jsonObject.toString());
+			audit.setAplTipoOperacion(action);
+			audit.setAplTabla(tableName);
+			session.getTransaction().begin();
+			session.save(audit);
+			session.getTransaction().commit();
+
+		}
+	}
+
+}
