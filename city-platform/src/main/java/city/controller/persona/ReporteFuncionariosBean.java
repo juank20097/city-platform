71e42a24bb070ad5c5427212cd1d11a0411b0e54
ricardo <ricardo@desarrollo>
2016-09-21 12:58:12 -0500

    Modificar bean para que ejecute filtros

--- city-platform/src/main/java/city/controller/persona/ReporteFuncionariosBean.java
+++ city-platform/src/main/java/city/controller/persona/ReporteFuncionariosBean.java
@@ -1,40 +1,48 @@
 package city.controller.persona;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 import city.controller.access.SesionBean;
 import city.model.dao.entidades.GenPersona;
+import city.model.dao.entidades.extras.DatosFuncionario;
 import city.model.dao.entidades.extras.Funcionario;
 import city.model.manager.ManagerReporteFuncionarios;
 
 @SessionScoped
 @ManagedBean
-public class ReporteFuncionariosBean {
+@ViewScoped
+public class ReporteFuncionariosBean implements Serializable {
 
 	@EJB
 	private ManagerReporteFuncionarios manager;
 	@Inject
 	private SesionBean session;
 
-	private List<Object[]> list;
+	private List<DatosFuncionario> funcionarios;
+	private List<DatosFuncionario> filteredFuncionarios;
 
-	private List<Funcionario> funcionarios;
 
 	@PostConstruct
 	public void ini() {
-		this.funcionarios = new ArrayList<Funcionario>();
+		this.funcionarios = new ArrayList<DatosFuncionario>();
+		cargarFuncionarios();
 
 	}
+	
+
 
 	public void cargarFuncionarios() {
-		this.setList(manager.test());
+		this.setFuncionarios(manager.getAllDatosFuncionarios());
 	}
 
 	private ManagerReporteFuncionarios getManager() {
@@ -53,20 +61,22 @@ public class ReporteFuncionariosBean {
 		this.session = session;
 	}
 
-	public List<Funcionario> getFuncionarios() {
+	public List<DatosFuncionario> getFuncionarios() {
 		return funcionarios;
 	}
 
-	private void setFuncionarios(List<Funcionario> funcionarios) {
+	public void setFuncionarios(ArrayList<DatosFuncionario> funcionarios) {
 		this.funcionarios = funcionarios;
 	}
-
-	private List<Object[]> getList() {
-		return list;
-	}
-
-	private void setList(List<Object[]> list) {
-		this.list = list;
-	}
+	
+	
+	public List<DatosFuncionario> getFilteredFuncionarios() {
+        return filteredFuncionarios;
+    }
+ 
+    public void setFilteredFuncionarios(List<DatosFuncionario> filteredFuncionarios) {
+        this.filteredFuncionarios = filteredFuncionarios;
+    }
+ 
 
 }
