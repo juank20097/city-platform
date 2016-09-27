dd4d90982717584f90e9d8ff6546d83c68b9f89f
luis <lvargas@yachay.gob.ec>
2016-09-27 16:27:14 -0500

    Excel

--- city-platform/src/main/java/city/controller/territorio/ReporteZonasBean.java
+++ city-platform/src/main/java/city/controller/territorio/ReporteZonasBean.java
@@ -1,5 +1,8 @@
 package city.controller.territorio;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +17,10 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
 
@@ -22,6 +29,9 @@ import city.model.dao.entidades.extras.DatosBarrios;
 import city.model.dao.entidades.extras.DatosDistrito;
 import city.model.dao.entidades.extras.DatosReporteTree;
 import city.model.dao.entidades.extras.DatosZona;
+import city.model.dao.entidades.extras.Externo;
+import city.model.generic.Funciones;
+import city.model.manager.ManagerCarga;
 import city.model.manager.ManagerReporteZonas;
 
 @SessionScoped
@@ -30,19 +40,31 @@ import city.model.manager.ManagerReporteZonas;
 public class ReporteZonasBean implements Serializable {
 
 	@EJB
+	private ManagerCarga managerC;
+	@EJB
 	private ManagerReporteZonas manager;
 	@Inject
 	private SesionBean session;
 
-	private TreeNode root;
+	private List<DatosReporteTree> zonas;
+	private HashMap<String, DatosReporteTree> listaVecindarios;
+	private HashMap<String, DatosReporteTree> listaDistritosM;
+	private List<DatosReporteTree> vecindarios;
 
-	// private List<DatosZona> filteredZonas;
+	private TreeNode root;
+	private DatosReporteTree selectedDocument;
+
+	private String url_doc;
 
 	@PostConstruct
 	public void ini() throws CloneNotSupportedException {
 		root = new DefaultTreeNode();
 		cargarDatos();
-
+		try {
+			url_doc = managerC.ParametroByID("direccion_doc");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public ReporteZonasBean() {
@@ -51,29 +73,16 @@ public class ReporteZonasBean implements Serializable {
 
 	public List<DatosZona> cargarDatos() throws CloneNotSupportedException {
 
-		List<DatosReporteTree> zonas;
-		List<DatosReporteTree> distritos;
-		List<DatosReporteTree> vecindarios;
-
 		zonas = manager.getAllElementosZonas();
-		// distritos = manager.getAllDatosDistritos();
 		vecindarios = manager.getAllDatosVecindarios();
 
 		HashMap<String, DefaultTreeNode> listaZonas = new HashMap<>();
 		int idZona = 0;
 		for (DatosReporteTree datosZonas : zonas) {
 			listaZonas.put(idZona++ + "", new DefaultTreeNode(datosZonas, root));
-
 		}
 
-		HashMap<String, DefaultTreeNode> listaDistritos = new HashMap<>();
-		// for (DatosReporteTree datosDistritos : distritos) {
-		// listaDistritos.put(datosDistritos.getId(),
-		// new DefaultTreeNode(datosDistritos,
-		// listaZonas.get(datosDistritos.getParentId())));
-		// }
-
-		HashMap<String, DatosReporteTree> listaVecindarios = new HashMap<>();
+		listaVecindarios = new HashMap<>();
 
 		for (DatosReporteTree vecindari : vecindarios) {
 
@@ -87,30 +96,64 @@ public class ReporteZonasBean implements Serializable {
 						.getElementoValor().equals("")) {
 					listaVecindarios.get(vecindarioZona.getParentId() + vecindarioZona.getElementoUnidadMedida())
 							.setElementoValor("");
-
+					listaVecindarios.get(vecindarioZona.getParentId() + vecindarioZona.getElementoUnidadMedida())
+							.setVecindarioHectareas("");
+					listaVecindarios.get(vecindarioZona.getParentId() + vecindarioZona.getElementoUnidadMedida())
+							.setVecindarioKilometros("");
 				}
 			}
 
-			double lastCantidad = 0;
-			double newCantidad = 0;
+			double lastCantidadZona = 0;
+			double newCantidadZona = 0;
+
+			double lastKilometrosZona = 0;
+			double newKilometrosZona = 0;
+
+			double lastHectareasZona = 0;
+			double newHectareasZona = 0;
 
 			if (!listaVecindarios.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida())
 					.getElementoValor().equals("")) {
-				lastCantidad = Double.parseDouble(listaVecindarios
+				lastCantidadZona = Double.parseDouble(listaVecindarios
 						.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida()).getElementoValor());
-
 			}
 
 			if (!vecindari.getElementoValor().equals("")) {
-				newCantidad = Double.parseDouble(vecindari.getElementoValor());
+				newCantidadZona = Double.parseDouble(vecindari.getElementoValor());
 			}
 
 			listaVecindarios.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida())
-					.setElementoValor((lastCantidad + newCantidad) + "");
+					.setElementoValor((lastCantidadZona + newCantidadZona) + "");
 
+			if (!listaVecindarios.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida())
+					.getVecindarioHectareas().equals("")) {
+				lastHectareasZona = Double.parseDouble(listaVecindarios
+						.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida()).getVecindarioHectareas());
+			}
+
+			if (!vecindari.getVecindarioHectareas().equals("")) {
+				newHectareasZona = Double.parseDouble(vecindari.getVecindarioHectareas());
+			}
+
+			listaVecindarios.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida())
+					.setVecindarioHectareas((lastHectareasZona + newHectareasZona) + "");
+
+			if (!listaVecindarios.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida())
+					.getVecindarioKilometros().equals("")) {
+				lastKilometrosZona = Double.parseDouble(
+						listaVecindarios.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida())
+								.getVecindarioKilometros());
+			}
+
+			if (!vecindari.getVecindarioKilometros().equals("")) {
+				newKilometrosZona = Double.parseDouble(vecindari.getVecindarioKilometros());
+			}
+
+			listaVecindarios.get(vecindario.getParentId() + vecindario.getElementoUnidadMedida())
+					.setVecindarioKilometros((lastKilometrosZona + newKilometrosZona) + "");
 		}
 
-		HashMap<String, DatosReporteTree> listaDistritosM = new HashMap<>();
+		listaDistritosM = new HashMap<>();
 
 		for (DatosReporteTree vecindari : vecindarios) {
 
@@ -125,40 +168,73 @@ public class ReporteZonasBean implements Serializable {
 						.getElementoValor().equals("")) {
 					listaDistritosM.get(vecindarioZona.getDistritoId() + vecindarioZona.getElementoUnidadMedida())
 							.setElementoValor("");
-
+					listaDistritosM.get(vecindarioZona.getDistritoId() + vecindarioZona.getElementoUnidadMedida())
+							.setVecindarioHectareas("");
+					listaDistritosM.get(vecindarioZona.getDistritoId() + vecindarioZona.getElementoUnidadMedida())
+							.setVecindarioKilometros("");
 				}
 			}
 
-			double lastCantidad = 0;
-			double newCantidad = 0;
+			double lastCantidadDistrito = 0;
+			double newCantidadDistrito = 0;
+
+			double lastHectareasDistrito = 0;
+			double newHectareasDistrito = 0;
+
+			double lastKilometrosDistrito = 0;
+			double newKilometrosDistrito = 0;
 
 			if (!listaDistritosM.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())
 					.getElementoValor().equals("")) {
-				lastCantidad = Double.parseDouble(listaDistritosM
+				lastCantidadDistrito = Double.parseDouble(listaDistritosM
 						.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida()).getElementoValor());
-
 			}
 
 			if (!vecindari.getElementoValor().equals("")) {
-				newCantidad = Double.parseDouble(vecindari.getElementoValor());
+				newCantidadDistrito = Double.parseDouble(vecindari.getElementoValor());
 			}
 
 			listaDistritosM.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())
-					.setElementoValor((lastCantidad + newCantidad) + "");
+					.setElementoValor((lastCantidadDistrito + newCantidadDistrito) + "");
 
+			if (!listaDistritosM.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())
+					.getVecindarioHectareas().equals("")) {
+				lastHectareasDistrito = Double.parseDouble(
+						listaDistritosM.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())
+								.getVecindarioHectareas());
+			}
+
+			if (!vecindari.getVecindarioHectareas().equals("")) {
+				newHectareasDistrito = Double.parseDouble(vecindari.getVecindarioHectareas());
+			}
+
+			listaDistritosM.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())
+					.setVecindarioHectareas((lastHectareasDistrito + newHectareasDistrito) + "");
+
+			if (!listaDistritosM.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())
+					.getVecindarioKilometros().equals("")) {
+				lastKilometrosDistrito = Double.parseDouble(
+						listaDistritosM.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())
+								.getVecindarioKilometros());
+			}
+
+			if (!vecindari.getVecindarioKilometros().equals("")) {
+				newKilometrosDistrito = Double.parseDouble(vecindari.getVecindarioKilometros());
+			}
+
+			listaDistritosM.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())
+					.setVecindarioKilometros((lastKilometrosDistrito + newKilometrosDistrito) + "");
 		}
 
 		for (Entry<String, DatosReporteTree> zona : listaVecindarios.entrySet()) {
 			DatosReporteTree value = zona.getValue();
 			listaZonas.put(value.getParentId() + value.getElementoUnidadMedida(), new DefaultTreeNode(value, root));
-
 		}
 
 		for (Entry<String, DatosReporteTree> zona : listaDistritosM.entrySet()) {
 			DatosReporteTree value = zona.getValue();
 			listaZonas.put(value.getDistritoId() + value.getElementoUnidadMedida(),
 					new DefaultTreeNode(value, listaZonas.get(value.getParentId() + value.getElementoUnidadMedida())));
-
 		}
 
 		for (DatosReporteTree vecindario : vecindarios) {
@@ -166,29 +242,6 @@ public class ReporteZonasBean implements Serializable {
 			listaZonas.put(vecindario.getVecindarioId() + vecindario.getElementoUnidadMedida(), new DefaultTreeNode(
 					vecindario, listaZonas.get(vecindario.getDistritoId() + vecindario.getElementoUnidadMedida())));
 		}
-
-		// for (Entry<String, DatosReporteTree> distrito :
-		// listaDistritoTemporal.entrySet()) {
-		// DatosReporteTree value = distrito.getValue();
-		// System.out.println("Distrito " + value.toString());
-		// listaDistritos.put(value.getDistritoId() +
-		// value.getElementoUnidadMedida(),
-		// new DefaultTreeNode(value, listaZonas.get(value.getParentId() +
-		// value.getElementoUnidadMedida())));
-		// }
-		//
-		// for (Entry<String, DatosReporteTree> vecindario :
-		// listaVecindarioTemporal.entrySet()) {
-		// DatosReporteTree value = vecindario.getValue();
-		// System.out.println("Vecindario " + value.toString());
-		// listaVecindarios.put(value.getVecindarioId(), new
-		// DefaultTreeNode(value,
-		// listaDistritos.get(value.getDistritoId() +
-		// value.getElementoUnidadMedida())));
-		// }
-		// listaZonas.get("ZONA1").ge;
-
-		// TreeNode zonas = new DefaultTreeNode(new D(), root);
 
 		return null;
 	}
@@ -217,12 +270,118 @@ public class ReporteZonasBean implements Serializable {
 		this.root = root;
 	}
 
-	// public List<DatosZona> getFilteredZonas() {
-	// return filteredZonas;
-	// }
-	//
-	// public void setFilteredZonas(List<DatosZona> filteredZonas) {
-	// this.filteredZonas = filteredZonas;
-	// }
+	public DatosReporteTree getSelectedDocument() {
+		return selectedDocument;
+	}
+
+	public void setSelectedDocument(DatosReporteTree selectedDocument) {
+		this.selectedDocument = selectedDocument;
+	}
+
+	public List<DatosReporteTree> getZonas() {
+		return zonas;
+	}
+
+	public void setZonas(List<DatosReporteTree> zonas) {
+		this.zonas = zonas;
+	}
+
+	public ManagerCarga getManagerC() {
+		return managerC;
+	}
+
+	public String getUrl_doc() {
+		return url_doc;
+	}
+
+	public void generateExcel() {
+		crearExcel(this.getZonas(), listaVecindarios, listaDistritosM, vecindarios);
+	}
+
+	public void crearExcel(List<DatosReporteTree> datosReporteTree, HashMap<String, DatosReporteTree> zonas,
+			HashMap<String, DatosReporteTree> distritosM, List<DatosReporteTree> vecindarios) {
+		String url = url_doc + "/descarga/";
+		try {
+
+			HSSFWorkbook libro = new HSSFWorkbook();
+			HSSFSheet hoja = libro.createSheet("Datos");
+			
+			int i = 0;
+			for (; i <= datosReporteTree.size() - 1; i++) {
+				HSSFRow row = hoja.createRow(i);
+				llenarFila(datosReporteTree.get(i), row);
+			}
+
+			for (Entry<String, DatosReporteTree> zona : zonas.entrySet()) {
+				HSSFRow row = hoja.createRow(i++);
+				DatosReporteTree value = zona.getValue();
+				llenarFila(zona.getValue(), row);
+				String zonaId = value.getParentId() + value.getElementoUnidadMedida();
+				for (Entry<String, DatosReporteTree> distrito : listaDistritosM.entrySet()) {
+					DatosReporteTree valueDistrito = distrito.getValue();
+
+					String distritoParentId = valueDistrito.getParentId() + valueDistrito.getElementoUnidadMedida();
+
+					if (distritoParentId.equals(zonaId)) {
+						HSSFRow rowDistrito = hoja.createRow(i++);
+						llenarFila(distrito.getValue(), rowDistrito);
+					}
+					String distritoId = zonaId + valueDistrito.getDistritoId()
+							+ valueDistrito.getElementoUnidadMedida();
+					for (DatosReporteTree vecindario : vecindarios) {
+
+						String vecindarioParentId = vecindario.getParentId() + vecindario.getElementoUnidadMedida()
+								+ vecindario.getDistritoId() + vecindario.getElementoUnidadMedida();
+	
+						if (vecindarioParentId.equals(distritoId)) {
+
+							HSSFRow rowVecindario = hoja.createRow(i++);
+							llenarFila(vecindario, rowVecindario);
+						}
+
+					}
+
+				}
+
+			}
+
+			OutputStream out = new FileOutputStream(url + "DatosExcel_Territorios.xls");
+			libro.write(out);
+			libro.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void llenarFila(DatosReporteTree datosReporteTree, HSSFRow row) {
+		if (row.getRowNum() == 0) {
+			HSSFCell celda0 = row.createCell(0);
+			celda0.setCellValue("NOMBRE");
+			HSSFCell celda1 = row.createCell(1);
+			celda1.setCellValue("KILÓMETROS");
+			HSSFCell celda2 = row.createCell(2);
+			celda2.setCellValue("HECTÁREAS");
+			HSSFCell celda3 = row.createCell(3);
+			celda3.setCellValue("ELEMENTO");
+			HSSFCell celda4 = row.createCell(4);
+			celda4.setCellValue("TIPO ELEMENTO");
+			HSSFCell celda5 = row.createCell(5);
+			celda5.setCellValue("VALOR");
+		} else {
+			HSSFCell celda0 = row.createCell(0);
+			celda0.setCellValue(datosReporteTree.getNombre());
+			HSSFCell celda1 = row.createCell(1);
+			celda1.setCellValue(datosReporteTree.getVecindarioKilometros());
+			HSSFCell celda2 = row.createCell(2);
+			celda2.setCellValue(datosReporteTree.getVecindarioHectareas());
+			HSSFCell celda3 = row.createCell(3);
+			celda3.setCellValue(datosReporteTree.getElementoNombre());
+			HSSFCell celda4 = row.createCell(4);
+			celda4.setCellValue(datosReporteTree.getElementoTipo());
+			HSSFCell celda5 = row.createCell(5);
+			celda5.setCellValue(datosReporteTree.getElementoValor());
+		}
+	}
 
 }
